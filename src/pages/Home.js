ed8da12430f64f5eb45adbfa1732d8f54f8d3b59
dannyr22 <dannyr2763@hotmail.com>
2021-03-02 19:14:12 +0000

    Game component added to list eah one after fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,53 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loadGames } from '../actions/gamesAction';
+//components
+import Game from '../components/Game';
+//Styles
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
 
 
 const Home = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(loadGames())
-  })
+  }, [dispatch]);
+
+  const { popular, upcoming, newGames } = useSelector(state => state.games);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <Games>
+        {upcoming.map(game => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id} 
+            key={game.id} 
+            image={game.background_image}
+         />
+        ))}
+      </Games>
+    </GameList>
   )
 }
 
+const GameList = styled(motion.div)`
+padding: 0rem 5rem;
+h2{
+  padding: 5rem 0rem;
+}
+`
+
+const Games = styled(motion.div)`
+min-height: 80vh;
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+grid-column-gap: 3rem;
+grid-row-gap: 5rem;
+`
+
 export default Home
+
