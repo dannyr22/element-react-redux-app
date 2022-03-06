dcd7ee56640705a5b2e4ca78cd6af739a9ca7bda
dannyr22 <dannyr2763@hotmail.com>
2022-03-06 18:03:49 +0000

    fixed api key

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import Home from './pages/Home';
-import GlobalStyles from './components/GlobalStyles';
-import { Route } from 'react-router-dom';
-import Nav from './components/Nav';
-
-
+import React from "react";
+import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
+import { Route } from "react-router-dom";
+import Nav from "./components/Nav";
+console.log(process.env.REACT_APP_API_KEY);
 
 function App() {
- 
   return (
     <div className="App">
       <GlobalStyles />
       <Nav />
-      <Route path={['/game/:id', '/']}>
+      <Route path={["/game/:id", "/"]}>
         <Home />
       </Route>
-      
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,50 +1,50 @@
 //BASE URL
 const base_url = "https://api.rawg.io/api/";
+const api_key = `${process.env.REACT_APP_API_KEY}`;
 
 //GETTING THE DATE
 const getCurrentMonth = () => {
   const month = new Date().getMonth() + 1;
   if (month < 10) {
-    return `0${month}`
+    return `0${month}`;
   } else {
-    return month
+    return month;
   }
-}
+};
 
 //GET CURRENT DATE
 const getCurrentDay = () => {
   const day = new Date().getDate();
   if (day < 10) {
-    return `0${day}`
+    return `0${day}`;
   } else {
-    return day
+    return day;
   }
-}
+};
 
 //CURRENT/DAY/MONTH/YEAR
-const currentYear = new Date().getFullYear()
-const currentMonth = getCurrentMonth()
-const currentDay = getCurrentDay()
-const currentDate = `${currentYear}-${currentMonth}-${currentDay}`
-const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`
-const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`
-
-
-
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //POPULAR GAMES
-const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?key=${api_key}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${api_key}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?key=${api_key}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
 //GAME DETAILS
-export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}?key=${api_key}`;
 //GAME SCREENSHOTS
-export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots`;
-
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?key=${api_key}`;
 
 //Searched Game
-export const searchedGameURL = (game_name) => `${base_url}games?search=${game_name}&page_size=9`
+export const searchedGameURL = (game_name) =>
+  `${base_url}games?key=${api_key}&search=${game_name}&page_size=9`;
